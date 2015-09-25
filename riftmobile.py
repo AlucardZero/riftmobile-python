beccc1f2f2aec43f0004f146fe1808e9923593e8
Chris Henning <saturos@gmail.com>
2015-09-25 13:15:45 -0400

    Implement secure login and character list

--- riftmobile.py
+++ riftmobile.py
@@ -1,6 +1,7 @@
 # Rift Mobile API
+# License: MIT (see LICENSE)
 import requests
-import json
+#import json
 import sys
 
 class Shard:
@@ -15,19 +16,43 @@ class Zone:
                 self.started = started
                 self.event = event
 
+class Character:
+       def __init__(self, name, playerId, shard, guild=None):
+                self.name = name
+                self.playerId = playerId
+                self.shard = shard
+                self.guild = guild
+
 class RiftClient:
         def __init__(self, dc):
                 if dc != "us" and dc != "eu":
                         sys.exit("Invalid DC\n");
                 self.url = "http://chat-" + dc + ".riftgame.com:8080/chatservice"
+                self.loginurl = "https://chat-" + dc + ".riftgame.com/chatservice/loginByTicket?os=iOS&osVersion=5.100000&vendor=Apple";
                 self.headers = { 'UserAgent': 'trion/mobile', 'Accept': 'application/json' }
+                self.session = None
 
         def sendRequest(self, path, payload=None):
-                r = requests.get(self.url + path, params=payload, headers=self.headers)
+                """ Sends REST request to the API
+                Uses a session if you have logged in
+
+                Args:
+                        path (string): string to append to the base API URL
+                        payload (dict): parameters to append to the GET or POST request
+
+                Returns:
+                        The response from the API in JSON format
+                """
+                r = None
+                if self.session != None:
+                        r = self.session.post(self.url + path, params=payload, headers=self.headers)
+                else:
+                        r = requests.post(self.url + path, params=payload, headers=self.headers)
+
                 if r.status_code != requests.codes.ok:
                         r.raise_for_status()
 
-                return r.json['data']
+                return r.json
 
         def listShards(self):
                 """ Lists all shards known to the mobile API.
@@ -36,7 +61,7 @@ class RiftClient:
                         A list of Shard objects
                 """
                 shards = self.sendRequest("/shard/list")
-                return map(lambda shard: Shard(shard['name'], shard['shardId']), shards)
+                return map(lambda shard: Shard(shard['name'], shard['shardId']), shards['data'])
                 
         def listZones(self, shardId):
                 """ Lists all zones known to the mobile API on a shard
@@ -48,4 +73,36 @@ class RiftClient:
                         A list of Zone objects
                 """
                 zones = self.sendRequest("/zoneevent/list", { 'shardId': shardId })
-                return map(lambda zone: Zone(zone['zone'], zone['zoneId'], zone.get('name'), zone.get('started')), zones)
+                return map(lambda zone: Zone(zone['zone'], zone['zoneId'], zone.get('name'), zone.get('started')), zones['data'])
+
+        def login(self, username, password):
+                """ Logs in to the mobile API.
+                Enables many more features
+
+                Args:
+                        username (string): The login userid
+                        password (string): The login password
+                Returns:
+                        A status code indicating the result. 0 = success, other = the HTTP error code
+                """
+                self.session = requests.Session()
+                r = self.session.post("https://auth.trionworlds.com/auth", params={'username': username, 'password': password, 'channel': 1}, headers=self.headers)
+                if r.status_code != requests.codes.ok:
+                        return r.status_code;
+
+                r = self.session.post("https://chat-us.riftgame.com/chatservice/loginByTicket?os=iOS&osVersion=5.100000&vendor=Apple", params={'ticket': r.text}, headers=self.headers)
+                if r.status_code != requests.codes.ok:
+                        return r.status_code
+                return 0
+
+        def listCharacters(self):
+                """ Gets the list of characters on your account
+                Must have logged in
+
+                Returns:
+                        a list of Character objects, or None if you have not logged in
+                """
+                if self.session == None:
+                        return None
+                characters = self.sendRequest("/chat/characters")
+                return map(lambda character: Character(character['name'], character['playerId'], character['shardName'], character.get('guild')), characters['data'])
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import riftmobile
+import sys
+import ConfigParser
 rc = riftmobile.RiftClient("us")
+
 # Print the 4th shard
 shards = rc.listShards()
 print shards[3].name
@@ -18,3 +21,14 @@ for shard in shards:
                         break
         if stop == True:
                 break
+
+# Start secured client
+config = ConfigParser.RawConfigParser()
+config.read('mobile.cfg')
+res = rc.login(config.get('Rift','username'), config.get('Rift','password'))
+if res != 0:
+        print "error:", res
+else:
+        res = rc.listCharacters()
+        # First character
+        print res[0].name + "@" + res[0].shard
